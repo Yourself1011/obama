4c3884dffe29b576045c609bbbf2179b11266ec4
Daniel Zhang <d.zhang200788@gmail.com>
2025-09-13 15:37:27 -0400

    lgpio

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -1,9 +1,9 @@
 import RPi.GPIO as GPIO
 import gpiozero
 
-pigpioFactory = gpiozero.pins.pigpio.PiGPIOFactory()
-# servo = gpiozero.AngularServo(5, min_angle=0, max_angle = 180, pin_factory=pigpioFactory)
-car = gpiozero.Robot(left=gpiozero.Motor(1, 2), right=gpiozero.Motor(3, 4))
+lgpioFactory = gpiozero.pins.lgpio.LGPIOFactory()
+# servo = gpiozero.AngularServo(5, min_angle=0, max_angle = 180, pin_factory=lgpioFactory)
+car = gpiozero.Robot(left=gpiozero.Motor(2, 3), right=gpiozero.Motor(4, 17))
 
 while True:
     car.forward()
