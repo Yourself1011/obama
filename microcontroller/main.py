976727b3bd2b9c17933a79840b831faeb61b9269
Daniel Zhang <d.zhang200788@gmail.com>
2025-09-13 14:02:07 -0400

    car

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -2,10 +2,11 @@ import RPi.GPIO as GPIO
 import gpiozero
 
 pigpioFactory = gpiozero.pins.pigpio.PiGPIOFactory()
-servo = gpiozero.AngularServo(3, min_angle=0, max_angle = 180, pin_factory=pigpioFactory)
+# servo = gpiozero.AngularServo(5, min_angle=0, max_angle = 180, pin_factory=pigpioFactory)
+car = gpiozero.Robot(left=gpiozero.Motor(1, 2), right=gpiozero.Motor(3, 4))
 
 while True:
-    servo.angle = 10
-    sleep(1)
-    servo.angle = 20
-    sleep(1)
+    car.forward()
+    sleep(2)
+    car.stop()
+    sleep(2)
